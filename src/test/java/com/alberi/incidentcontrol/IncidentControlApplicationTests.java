5e6f53fd9d16b468163b15d0564c3132efafa47f
Alberi Lemos <alberi.depaula@gmail.com>
2022-03-10 16:59:50 -0300

    IMPLEMENTAÇÃO DOS TESTES UNITÁRIOS

--- src/test/java/com/alberi/incidentcontrol/IncidentControlApplicationTests.java
+++ src/test/java/com/alberi/incidentcontrol/IncidentControlApplicationTests.java
@@ -1,13 +1,45 @@
 package com.alberi.incidentcontrol;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.alberi.incidentcontrol.model.Incident;
+import com.alberi.incidentcontrol.repository.IncidentRepository;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IncidentControlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @Autowired
+    private IncidentRepository incidentRepo;
+
+    @Test
+    void create() throws Exception {
+        Incident incident = new Incident();
+        incident.setId(1);
+        incident.setName("Teste 001");
+        incident.setDescription("description Teste 001");
+        incident.setCreatedAt(System.currentTimeMillis());
+        incidentRepo.save(incident);
+        assertNotNull(incidentRepo.findById(1).get());
+    }
+
+    @Test
+    void delete() throws Exception {
+        Incident incident = new Incident();
+        incident.setId(1);
+        incident.setName("Teste 001");
+        incident.setDescription("description Teste 001");
+        incident.setCreatedAt(System.currentTimeMillis());
+        incidentRepo.delete(incident);
+        assertEquals(incidentRepo.findById(1).get(), null);
+    }
 
 }
