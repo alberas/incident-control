a6cf5bb39e7f6d908ac1023f8cb07c386258a9db
Alberi Lemos <alberi.depaula@gmail.com>
2022-03-10 17:30:47 -0300

    IMPLEMENTAÇÃO DOS TESTES DA REST API

--- src/test/java/com/alberi/incidentcontrol/IncidentControlApplicationTestsRest.java
+++ src/test/java/com/alberi/incidentcontrol/IncidentControlApplicationTestsRest.java
@@ -1,17 +1,14 @@
 package com.alberi.incidentcontrol;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.alberi.incidentcontrol.controller.IncidentController;
 import com.alberi.incidentcontrol.model.Incident;
-import com.alberi.incidentcontrol.repository.IncidentRepository;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -21,10 +18,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(IncidentController.class)
 class IncidentControlApplicationTestsRest {
-
-    @Test
-    void contextLoads() {
-    }
 
     @Autowired
     private MockMvc mvc;
@@ -37,7 +30,7 @@ class IncidentControlApplicationTestsRest {
         incident.setDescription("description Teste 001");
         incident.setCreatedAt(System.currentTimeMillis());
 
-        RequestBuilder request = MockMvcRequestBuilders.post("/creade", incident.toString());
+        RequestBuilder request = MockMvcRequestBuilders.post("/create", incident.toString());
         MvcResult result = mvc.perform(request).andReturn();
         assertEquals(incident.toString(), result.getResponse().getContentAsString());
     }
