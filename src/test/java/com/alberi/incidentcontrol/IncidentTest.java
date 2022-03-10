c1462df21682a79b34f7c2082f59331e54ad71d9
Alberi Lemos <alberi.depaula@gmail.com>
2022-03-10 16:54:39 -0300

    IMPLEMENTAÇÃO DOS TESTES UNITÁRIOS

--- src/test/java/com/alberi/incidentcontrol/IncidentTest.java
+++ src/test/java/com/alberi/incidentcontrol/IncidentTest.java
@@ -10,14 +10,8 @@ import com.alberi.incidentcontrol.repository.IncidentRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(IncidentController.class)
